8bc2ac94930bef985cbcaaa017e355bb98416e15
liuxuan <liuxuan@test.com>
2020-12-23 19:40:57 +0800

    feat: 生产环境开启CSS和zip压缩

--- webpack/plugins.js
+++ webpack/plugins.js
@@ -1,17 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
+const cssnano = require('cssnano');
 const _MiniCssExtractPlugin = require('mini-css-extract-plugin'); // eslint-disable-line
 const _StyleLintPlugin = require('stylelint-webpack-plugin'); // eslint-disable-line
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'); // css 压缩
+const ZipWebpackPlugin = require('zip-webpack-plugin');
 const { htmlWebpackPlugins } = require('./path.config');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer'); // 优化体积的时候启用
 
-// const isDev = process.env.NODE_ENV === 'development';
+const production = process.env.NODE_ENV === 'production';
 
 const MiniCssExtractPlugin = new _MiniCssExtractPlugin({
   filename: 'css/[name][contenthash:8].css',
-  // chunkFilename: '[id].css',
+  chunkFilename: '[id].css',
 });
 
 const StyleLintPlugin = new _StyleLintPlugin({
@@ -20,7 +23,7 @@ const StyleLintPlugin = new _StyleLintPlugin({
   files: '**/*.less',
 });
 
-const plugins = [
+const commonPlugins = [
   new webpack.ProgressPlugin(),
   MiniCssExtractPlugin,
   StyleLintPlugin,
@@ -29,4 +32,16 @@ const plugins = [
   // new BundleAnalyzerPlugin(),
 ].concat(htmlWebpackPlugins);
 
-module.exports = plugins;
+const prodPlugins = production
+  ? [
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: cssnano,
+    }),
+    new ZipWebpackPlugin({
+      filename: 'business.zip',
+    }),
+  ]
+  : [];
+
+module.exports = commonPlugins.concat(prodPlugins);
